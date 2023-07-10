78dcf586e226c7d030e060788ef7305ffbda6336
kristian tomic <kt99kiki@gmail.com>
2023-07-10 13:40:41 +0200

    Benchmark remove

--- BenckmarkApp/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿
-
-using BenchmarkDotNet.Running;
-using BenckmarkApp;
-
-Console.WriteLine("tu sam");
-var summary = BenchmarkRunner.Run<DiplomskiRadBenchmark>();
-Console.WriteLine(summary.ToString());
-Console.ReadKey();
